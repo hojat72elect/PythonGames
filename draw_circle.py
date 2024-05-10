ace14df4d7d1413fb47820280b34060a9f5028b1
hojat <hojat72elect@gmail.com>
2024-05-09 23:38:30 -0400

    Initial commit

---
+++ draw_circle.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame import Surface, SurfaceType
+
+"""
+Just draws a circle on the screen where mouse is clicked.
+"""
+
+window_size: tuple[int, int] = (500, 400)
+screen: Surface | SurfaceType = pygame.display.set_mode(window_size)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            position: tuple[int, int] = pygame.mouse.get_pos()
+            color: tuple[int, int, int] = (255, 255, 255)
+            pygame.draw.circle(screen, color, position, 10, 3)
+            pygame.display.update()
