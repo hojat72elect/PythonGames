41865c1e038e6affab9c569f452a6978cd20bdd3
hojat <hojat72elect@gmail.com>
2024-05-22 02:17:31 -0400

    Refactored previous examples of python.

--- src/pygame_examples/example1_draw_pixel.py
+++ src/pygame_examples/example1_draw_pixel.py
@@ -1,23 +1,23 @@
 import pygame
-from pygame import Surface, SurfaceType, Color, display, quit, event, QUIT
+
 
 # Create the window
 pygame.init()
 screen_width: int = 1_000
 screen_height: int = 800
-screen: Surface | SurfaceType = display.set_mode((screen_width, screen_height))
+screen: pygame.Surface | pygame.SurfaceType = pygame.display.set_mode((screen_width, screen_height))
 
 is_finished: bool = False
-white: Color = Color(255, 255, 255)
+white: pygame.Color = pygame.Color(255, 255, 255)
 
 while not is_finished:
-    for game_event in event.get():
+    for game_event in pygame.event.get():
         # Will close the window by clicking the close button
-        if game_event.type == QUIT:
+        if game_event.type == pygame.QUIT:
             is_finished = True
     # Sets a specific pixel to white
     screen.set_at((100, 400), white)
     # Updates the display screen
-    display.update()
+    pygame.display.update()
 
-quit()
+pygame.quit()
--- src/pygame_examples/example2_draw_squares.py
+++ src/pygame_examples/example2_draw_squares.py
@@ -1,24 +1,24 @@
 """
 Draws several squares on the screen.
 """
-from pygame import init, display, Color, draw, event, QUIT, SurfaceType, Surface
+import pygame
 
-init()
+pygame.init()
 screen_width: int = 900
 screen_height: int = 500
-screen: Surface | SurfaceType = display.set_mode((screen_width, screen_height))
+screen: pygame.Surface | pygame.SurfaceType = pygame.display.set_mode((screen_width, screen_height))
 
 is_finished: bool = False
-white: Color = Color(255, 255, 255)
+white: pygame.Color = pygame.Color(255, 255, 255)
 
 
 def draw_square(x: int, y: int, size: int):
-    draw.rect(screen, white, (x, y, size, size))
+    pygame.draw.rect(screen, white, (x, y, size, size))
 
 
 while not is_finished:
-    for game_event in event.get():
-        if game_event.type == QUIT:
+    for game_event in pygame.event.get():
+        if game_event.type == pygame.QUIT:
             is_finished = True
 
     draw_square(100, 100, 10)
@@ -31,6 +31,6 @@ while not is_finished:
     draw_square(653, 165, 40)
     draw_square(773, 102, 20)
     draw_square(822, 153, 30)
-    display.update()
+    pygame.display.update()
 
-quit()
+pygame.quit()
